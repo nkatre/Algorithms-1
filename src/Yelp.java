1ce1b96f002a302680ea0e97cdf8f80899daf607
Shubham Shukla <shubhamshukla7@gmail.com>
2013-09-29 01:23:10 -0700

    Playing with Yelp APIs

---
+++ src/Yelp.java
@@ -0,0 +1,72 @@
+/*
+Example code based on code from Nicholas Smith at http://imnes.blogspot.com/2011/01/how-to-use-yelp-v2-from-java-including.html
+For a more complete example (how to integrate with GSON, etc) see the blog post above.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.model.OAuthRequest;
+import org.scribe.model.Response;
+import org.scribe.model.Token;
+import org.scribe.model.Verb;
+import org.scribe.oauth.OAuthService;
+
+/**
+* Example for accessing the Yelp API.
+*/
+public class Yelp {
+
+  OAuthService service;
+  Token accessToken;
+
+  /**
+* Setup the Yelp API OAuth credentials.
+*
+* OAuth credentials are available from the developer site, under Manage API access (version 2 API).
+*
+* @param consumerKey Consumer key
+* @param consumerSecret Consumer secret
+* @param token Token
+* @param tokenSecret Token secret
+*/
+  public Yelp(String consumerKey, String consumerSecret, String token, String tokenSecret) {
+    this.service = new ServiceBuilder().provider(YelpApi2.class).apiKey(consumerKey).apiSecret(consumerSecret).build();
+    this.accessToken = new Token(token, tokenSecret);
+  }
+
+  /**
+* Search with term and location.
+*
+* @param term Search term
+* @param latitude Latitude
+* @param longitude Longitude
+* @return JSON string response
+*/
+  public String search(String term, double latitude, double longitude) {
+    OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.yelp.com/v2/search");
+    request.addQuerystringParameter("term", term);
+    request.addQuerystringParameter("ll", latitude + "," + longitude);
+    this.service.signRequest(this.accessToken, request);
+    Response response = request.send();
+    return response.getBody();
+  }
+
+  // CLI
+  public static void main(String[] args) {
+    // Update tokens here from Yelp developers site, Manage API access.
+    String consumerKey = "1q8hPyWy5UUS5jhGU-uuTw";
+    String consumerSecret = "3dMDDCVKtn6gsKgPfQTg2CPV0hE";
+    String token = "4I-CByb0aZq-vhOZU-IxiegvBMHa_YKz";
+    String tokenSecret = "O2s-rUWr3smo_tGQkiQrN24vGyk";
+
+    Yelp yelp = new Yelp(consumerKey, consumerSecret, token, tokenSecret);
+    String response = yelp.search("burritto", 47.621, -122.3331);
+
+    System.out.println(response);
+  }
+}
